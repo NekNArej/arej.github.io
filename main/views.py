681f903c96a1bd91f9d48458928ba8f682183451
NekNArej <167296730+NekNArej@users.noreply.github.com>
2024-04-17 16:53:28 +0300

    Delete main directory

--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-# def index(request):
-#     return HttpResponse("<h4>ПРОВЕРКА РАБОТЫ</h4>")
-def index(request):
-    return render(request,'main/index.html')
-def about(request):
-    return render(request,'main/about.html')
